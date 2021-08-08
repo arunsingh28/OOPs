f05191cd0ea3b954dfccf33926e1510c06c81094
Arun Singh <arunsingh28aug.as@gmail.com>
2021-08-09 01:04:30 +0530

    Inheritance

--- main.cpp
+++ main.cpp
@@ -9,9 +9,11 @@ class AbstractEmp
 class Employee : AbstractEmp
 {
 private:
-    string Name;
     string Company;
     int Age;
+
+protected:
+    string Name;
 
 public:
     // setter
@@ -68,14 +70,46 @@ public:
 
     void AskForPromotion()
     {
-        if(Age > 30){
-            std::cout<<Name <<" got promoted!"<< std::endl;
-        }else{
-            std::cout<<Name <<" sorry not promoted!"<< std::endl;
+        if (Age > 30)
+        {
+            std::cout << Name << " got promoted!" << std::endl;
+        }
+        else
+        {
+            std::cout << Name << " sorry not promoted!" << std::endl;
         }
     }
 };
 
+// Inheritance from Employee class
+class Developer : public Employee
+{
+public:
+    string FavLang;
+
+    Developer(string name, string company, int age, string favlang) : Employee(name, company, age)
+    {
+        FavLang = favlang;
+    }
+    void FixBug()
+    {
+        std::cout << getName() << " Fixed bug using " << FavLang << std::endl;
+    }
+};
+
+class Teacher : public Employee
+{
+public:
+    string Subject;
+    void PrepareLesson()
+    {
+        std::cout << Name << " is preparing " << Subject << " lesson" << std::endl;
+    }
+    Teacher(string name, string company, int age, string subject) : Employee(name, company, age)
+    {
+        Subject = subject;
+    }
+};
 int main()
 {
     Employee emp = Employee("AS Gamer", "Youtube", 21);
@@ -85,7 +119,14 @@ int main()
     // cout << "\n";
     // emp1.Intro();
 
+    // emp.AskForPromotion();
+    // emp1.AskForPromotion();
 
-    emp.AskForPromotion();
-    emp1.AskForPromotion();
+    // Developer dev("Arun Singh", "Freelancer", 22, "javascript");
+    // dev.FixBug();
+    // dev.AskForPromotion();
+
+    Teacher t = Teacher("Arun Singh", "Freelancer", 40, "History");
+    t.PrepareLesson();
+    t.AskForPromotion();
 }
