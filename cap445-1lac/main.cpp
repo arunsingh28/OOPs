2a8347b42ca8a372e5ae3263f45b45fcb74fa177
Arun Singh <arunsingh28aug.as@gmail.com>
2021-09-06 16:27:00 +0530

    cap445

---
+++ cap445-1lac/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int 
+}
+
+int bill()
+{
+    int bill=0;
+    cout << "Calculate Bill\n Enter Your units:";
+    cin >> bill;
+    if(bill>=100){
+        cout << "Your bill is:" << bill * 11;
+    }
+    cout << "Your Bill is:"<<bill * 5.69;
+    return 0;
+}
